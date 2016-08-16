7502133d7a5545ef53a25f2a20ff7f5d1c4e05df
houyong <houyong@zlebank.com>
2016-08-16 18:24:13 +0800

    修改测试方法
    Signed-off-by: houyong <houyong@zlebank.com>

---
+++ zplatform-rmi-client/src/test/java/com/zlebank/zplatform/rmi/test/member/MemberAccountService.java
@@ -0,0 +1,47 @@
+/* 
+ * MemberAccountService.java  
+ * 
+ * version TODO
+ *
+ * 2016年8月16日 
+ * 
+ * Copyright (c) 2016,zlebank.All rights reserved.
+ * 
+ */
+package com.zlebank.zplatform.rmi.test.member;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alibaba.fastjson.JSONObject;
+import com.zlebank.zplatform.acc.bean.enums.Usage;
+import com.zlebank.zplatform.member.bean.MemberAccountBean;
+import com.zlebank.zplatform.member.bean.MemberBean;
+import com.zlebank.zplatform.member.bean.enums.MemberType;
+import com.zlebank.zplatform.rmi.test.BaseTest;
+import com.zlebank.zplatform.rmi.test.IMemberAccountService;
+
+/**
+ * Class Description
+ *
+ * @author houyong
+ * @version
+ * @date 2016年8月16日 下午6:20:21
+ * @since 
+ */
+public class MemberAccountService extends BaseTest {
+    @Autowired
+    private IMemberAccountService memberAccountService;
+    @Test
+    public void test(){
+        try {
+            MemberBean member=new MemberBean();
+            member.setMemberId("200000000000622");
+            MemberAccountBean result= memberAccountService.queryBalance(MemberType.INDIVIDUAL, member, Usage.BASICPAY);
+            System.out.println(JSONObject.toJSONString(result));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
+    }
+}
