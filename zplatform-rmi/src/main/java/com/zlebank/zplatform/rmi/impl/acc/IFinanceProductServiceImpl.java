e8d7c88c66b5d5cc2de01ec7690a9d27195489ee
houyong <houyong@zlebank.com>
2016-08-26 10:21:10 +0800

    修改产品开通接口
    Signed-off-by: houyong <houyong@zlebank.com>

--- zplatform-rmi/src/main/java/com/zlebank/zplatform/rmi/impl/acc/IFinanceProductServiceImpl.java
+++ zplatform-rmi/src/main/java/com/zlebank/zplatform/rmi/impl/acc/IFinanceProductServiceImpl.java
@@ -10,27 +10,13 @@
  */
 package com.zlebank.zplatform.rmi.impl.acc;
 
-import java.util.List;
 
-import org.apache.commons.lang.exception.ExceptionUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.zlebank.zplatform.acc.bean.BusiAcct;
 import com.zlebank.zplatform.acc.bean.FinanceProductBean;
-import com.zlebank.zplatform.acc.dao.FinanceProductDAO;
-import com.zlebank.zplatform.acc.pojo.PojoBusiAcctSubjectMapping;
-import com.zlebank.zplatform.acc.pojo.PojoFinanceProduct;
-import com.zlebank.zplatform.acc.service.BusiAcctService;
 import com.zlebank.zplatform.acc.service.FinanceProductService;
-import com.zlebank.zplatform.acc.service.SubjectSelector;
-import com.zlebank.zplatform.acc.service.impl.FinanceProductActor;
-import com.zlebank.zplatform.member.bean.BusinessActor;
-import com.zlebank.zplatform.member.bean.enums.BusinessActorType;
+import com.zlebank.zplatform.rmi.interfaces.acc.IFinanceProductService;
 
 /**
  * 金融产品
@@ -41,52 +27,15 @@ import com.zlebank.zplatform.member.bean.enums.BusinessActorType;
  * @since 
  */
 @Service
-public class IFinanceProductServiceImpl implements FinanceProductService {
-    private Log log = LogFactory.getLog(IFinanceProductServiceImpl.class);
+public class IFinanceProductServiceImpl implements IFinanceProductService {
     @Autowired
-    private SubjectSelector subjectSelector;
-    @Autowired
-    private BusiAcctService busiAcctService;
-    @Autowired
-    private FinanceProductDAO financeProductDao;
+    private FinanceProductService financeProductService;
     /**
      *
      * @param bean
      * @throws Exception 
      */
-    @Override
-    @Transactional(propagation=Propagation.REQUIRED,rollbackFor=Throwable.class)
     public void openProduct(FinanceProductBean bean,long userId) throws Exception {
-        try {
-            openProductAccout(bean, userId);
-            saveProduct(bean,userId);
-        } catch (Exception e) {
-            log.error(ExceptionUtils.getFullStackTrace(e));
-            throw e;
-        }
-    }
-    /**
-     * @param bean
-     * @param userId
-     */
-    private void saveProduct(FinanceProductBean bean, long userId) {
-            PojoFinanceProduct pojoFinanceProduct=new PojoFinanceProduct();
-            pojoFinanceProduct.setFinancier(bean.getFinancier());
-            pojoFinanceProduct.setFundManager(bean.getFundManager());
-            pojoFinanceProduct.setProductCode(bean.getProductCode());
-            pojoFinanceProduct.setProductName(bean.getProductName());
-            financeProductDao.saveA(pojoFinanceProduct);
-    }
-    private void openProductAccout(FinanceProductBean bean,long userId) throws Exception{
-        BusinessActor prouductActor = new FinanceProductActor(bean);
-        List<PojoBusiAcctSubjectMapping> busiAcctSubjectMappings = subjectSelector
-                .getDefaultList(BusinessActorType.PRODUCT);
-        BusiAcct busiAcct;
-        for (PojoBusiAcctSubjectMapping busiAcctSubjectMappin : busiAcctSubjectMappings) {
-            busiAcct = new BusiAcct();
-            busiAcct.setBusiAcctName(bean.getProductName());
-            busiAcct.setUsage(busiAcctSubjectMappin.getUsage());
-            busiAcctService.openBusiAcct(prouductActor, busiAcct,userId);
-        }
+            financeProductService.openProduct(bean, userId);
     }
 }
